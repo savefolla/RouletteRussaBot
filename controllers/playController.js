bff9540ec5f22f3978bbdd87b2e82926c8e1f7b7
savefolla <savefolla@gmail.com>
2017-08-22 17:22:26 +0200

    update con timeout

--- controllers/playController.js
+++ controllers/playController.js
@@ -9,18 +9,26 @@ class PlayController extends Telegram.TelegramBaseController {
 		const x = Math.floor(Math.random()*6);
 		const y = Math.floor(Math.random()*6);
 		console.log(x+' vs '+y);
-		if(x == y){
-			$.sendMessage($.message.from.username + ' è morto per fortuna');
-			$.kickChatMember($.message.from.id).then(
-				$.unbanChatMember($.message.from.id).then(
-					console.log($.message.from.username+' ('+$.message.from.id+') è stato kickato e sbannato')
-				)
-			);
-			
-		}else{
-			console.log($.message.from.username+' ('+$.message.from.id+') è sopravvissuto');
-			$.sendMessage($.message.from.username + ' è sopravvissuto purtroppo');
-		}
+		const wait = Math.floor(Math.random()*1000*60);
+		console.log('aspetto '+wait+' millisecondi');
+		$.sendMessage($.message.from.username + ' saprai il tuo destino fra '+Math.floor(wait/1000)+' secondi');
+		setTimeout(() => {
+			if(x == y){
+				const time = Math.floor(Math.random()*1000*60);		
+				$.sendMessage($.message.from.username + ' ti restano '+Math.floor(time/1000)+' secondi in questo gruppo. Spendili bene');
+				setTimeout(() => {
+					$.sendMessage($.message.from.username + ' è morto per fortuna');
+					$.kickChatMember($.message.from.id).then(
+						$.unbanChatMember($.message.from.id).then(
+							console.log($.message.from.username+' ('+$.message.from.id+') è stato kickato e sbannato')
+						)
+					);
+				}, time);					
+			}else{
+				console.log($.message.from.username+' ('+$.message.from.id+') è sopravvissuto');
+				$.sendMessage($.message.from.username + ' è sopravvissuto purtroppo');
+			}
+		}, wait);		
 	}
 	get routes() {
 		return {
